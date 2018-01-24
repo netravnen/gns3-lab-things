3045b306ded92fd59908f7cff109fc3c69c99c81
Christoffer <netravnen@gmail.com>
2018-01-24 09:07:19 +0100

    Update archive.cmd

--- images/archive.cmd
+++ images/archive.cmd
@@ -1,3 +1,3 @@
 for /F "DELIMS=" %%I IN ('dir /b /s *.iso *.image *.bin') DO (
-    7z a -t7z -mx9 -mmt=on -myx=9 -sdel -x!*.7z "%%~dpI%%~nI%%~xI.7z" "%%I"
+    7z a -t7z -mx9 -mmt=on -myx=9 -sdel -x!*.7z "%%I.7z" "%%I"
 )
